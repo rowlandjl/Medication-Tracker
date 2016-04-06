d9bcd0edd1430d34bbbdb03d01907676fc47675a
rowlandjl <rowlandjl@vcu.edu>
2016-04-06 17:56:00 -0400

    rspec updates

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name "Jane"
+    last_name "Doe"
+    dob "1/1/1947"
+    email
+    password
+  end
+
+  sequence :email do |n|
+    "person#{n}@test.com"
+  end
+
+  sequence :password do |n|
+    "#{n}password"
   end
 end
