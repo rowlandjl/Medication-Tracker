211e18ce9b5530ccd1875db2a39ad4c66acc7c7f
rowlandjl <rowlandjl@vcu.edu>
2016-04-13 21:55:34 -0400

    prescriptions rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'welcome_pages#index'
 
   devise_for :users
+
+  resource :prescriptions
 end
--- spec/factories/prescriptions.rb
+++ spec/factories/prescriptions.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :prescription do
-    
+    drug_name "Crestor"
+    quantity "30"
+    dose_count "1"
+    frequency "once a day"
+    start_date "1/1/2016"
+    physician_name "John Doe"
   end
 end
