d0a7751d1e99ff92d8f5390046ec0395f19c2397
rowlandjl <rowlandjl@vcu.edu>
2016-04-14 22:09:39 -0400

    create dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resource :dashboard, only: [:show]
+
+  authenticated :user do
+    root 'dashboards#show'
+  end
+
   root 'welcome_pages#index'
 
-  devise_for :users
 end
