f27a68249973dd9d1ba0bd62b548f6e9debe1f1e
rowlandjl <rowlandjl@vcu.edu>
2016-04-24 11:31:02 -0400

    update forms and css

--- db/migrate/20160424023043_remove_column_physician_name.rb
+++ db/migrate/20160424023043_remove_column_physician_name.rb
@@ -1,6 +1,6 @@
 class RemoveColumnPhysicianName < ActiveRecord::Migration
   def change
     remove_column :prescriptions, :physician_name, :string
-    add_column :prescriptions, :physician_id, :integer, null: false
+    add_column :prescriptions, :physician_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160424023043) do
     t.integer  "user_id",      null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "physician_id", null: false
+    t.integer  "physician_id"
   end
 
   create_table "users", force: :cascade do |t|
