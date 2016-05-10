2aa89c399a9090632ed86e45c1ddc22ac5a97398
rowlandjl <rowlandjl@vcu.edu>
2016-05-10 13:39:48 -0400

    remove dotenv

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
-HOSTNAME = ENV['HOSTNAME']
-
 module MedicationTracker
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
