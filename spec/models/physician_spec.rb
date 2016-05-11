357b4d9555ca292752debc582b1d9d14cef2618c
rowlandjl <rowlandjl@vcu.edu>
2016-05-10 20:46:04 -0400

    fixed spacing on tests

--- spec/models/physician_spec.rb
+++ spec/models/physician_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Physician do
   it { should belong_to(:user) }
-  it { should have_many(:prescriptions)}
+  it { should have_many(:prescriptions) }
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:phone) }
